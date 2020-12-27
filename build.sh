661f54e05ca634847003f5752a87cd9fb73b61a0
ksmai <kasingmai@gmail.com>
2020-12-27 23:07:44 +0800

    Fix permission issue in dist/ folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t fire-web-engine .
-docker run --rm -v $(pwd):/src fire-web-engine "$@"
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) fire-web-engine "$@"
