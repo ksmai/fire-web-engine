5f2810835c131f2ed88359ef918968d46fe44e6f
ksmai <kasingmai@gmail.com>
2021-02-06 20:01:53 +0800

    Use Makefile to build resources.zip for demo

--- scripts/update_demo.sh
+++ scripts/update_demo.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-./scripts/build.sh && cp dist/index.js dist/index.wasm demo/
-cd demo/
-rm resources.zip
-zip resources.zip \
-  levels.xml \
-  music.ogg \
-  roguelikeSheet_transparent.png \
-  SpriteSheets.xml
-cd ..
+./scripts/build.sh && \
+cp dist/index.js dist/index.wasm demo/ && \
+cd demo/ && make && cd ..
