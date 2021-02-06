1206f81ddda5c5e05784dd7912a683e9bca2eecf
ksmai <kasingmai@gmail.com>
2021-02-06 11:09:16 +0800

    Remove temp testing code

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -2,76 +2,10 @@
 #include "App/App.h"
 #include "App/Logger.h"
 
-// for temp testing
-#include "File/ZipFile.h"
-#include "File/ImageFile.h"
-#include "File/XMLFile.h"
-#include "Graphics/Color.h"
-#include "Actor/ActorID.h"
-#include <iostream>
-#include <functional>
-
 bool FW::App::created{false};
 
-struct MyData1 {
-  int x, y;
-
-  MyData1(int x, int y): x{x}, y{y} {}
-};
-
-struct MyData2 {
-  std::string secret;
-};
-
-void myListener1(const MyData1& d) {
-  std::cout << "myListener1 called with " << d.x << " " << d.y << "\n";
-}
-
-void myListener2(const MyData2& d) {
-  std::cout << "myListener2 called with " << d.secret << "\n";
-}
-
-void myListener3(const MyData1& d) {
-  std::cout << "myListener3 called with " << d.x << " " << d.y << "\n";
-}
-
-struct MyListenerObject {
-  void myListenMethod(const MyData2& d) {
-    std::cout << "MyListenerObject::myListenMethod called with " << d.secret << "\n";
-  }
-};
-
-MyListenerObject obj;
-
-class TestClassForLua {
-public:
-  explicit TestClassForLua(int secret):
-    secret{secret}
-  {
-    std::cout << "TestClassForLua(" << secret << ") called\n";
-  }
-
-  double someMethod(const char* s, unsigned long long x) {
-    std::cout << "TestClassForLua::someMethod(\"" << s << "\", " << x << "uLL) called\n";
-    return x / 3.0 + secret;
-  }
-
-private:
-  int secret;
-};
-
-void useTestClassForLua(TestClassForLua* ptr) {
-  ptr->someMethod("calling useTestClassForLua", 444444);
-}
-
-void useMyData1FromLua(MyData1* ptr) {
-  std::cout << "Received MyData1 from Lua! " << ptr->x << ", " << ptr->y << "\n";
-  std::swap(ptr->x, ptr->y);
-}
-
 FW::App::App(const Config& config):
-  graphics{config.title, config.canvasWidth, config.canvasHeight},
-  remoteFile{"/resources.zip"}
+  graphics{config.title, config.canvasWidth, config.canvasHeight}
 {
   if (created) {
     Logger::error("Cannot create more than 1 App");
@@ -82,8 +16,6 @@ FW::App::App(const Config& config):
     Logger::error("SDL_Init() failed: %s", SDL_GetError());
     abort();
   }
-
-  remoteFile.open();
 }
 
 FW::App::~App() {
@@ -91,176 +23,10 @@ FW::App::~App() {
   created = false;
 }
 
-void FW::App::init() {
-  if (initialized) {
-    return;
-  }
-  if (remoteFile.isError()) {
-    Logger::error(remoteFile.getError().c_str());
-    abort();
-  }
-  if (!remoteFile.isOpened()) {
-    return;
-  }
-
-  // for temp testing
-  // texture
-  graphics.setClearColor(FW::Color{0.7f, 0.42f, 0.66f, 1.0f});
-  ZipFile zipFile{remoteFile.getData()};
-  remoteFile.close();
-  spriteSheet.reset(new SpriteSheet{graphics.makeSpriteSheet(zipFile.getFileContent("roguelikeSheet_transparent.png"), 16, 16, 1)});
-  SpritePosition pos{13, 12, 4, 4};
-  sprite.reset(new Sprite{spriteSheet->makeSprite(pos)});
-
-  // xml
-  XMLFile xmlFile{zipFile.getFileContent("test.xml")};
-  std::cout << "test xmlFile:\n";
-  std::cout << xmlFile.root().find("Character").size() << "\n";
-  std::cout << xmlFile.root().find("Shop").size() << "\n";
-  std::cout << xmlFile.root().find("Enemy").size() << "\n";
-  std::cout << xmlFile.root().find("Character")[0].find("Weapon")[0].attr("name") << "\n"; 
-  const char* attr = xmlFile.root().find("Character")[0].find("Weapon")[0].attr("non"); 
-  std::cout << (attr ? attr : "CANNOT FIND") << "\n";
-
-  // test audio
-  sound.reset(new Sound{audio.loadSound(zipFile.getFileContent("coin.wav"))});
-  music.reset(new Music{audio.loadMusic(zipFile.getFileContent("music.ogg"))});
-  // audio.playMusic(*music);
-
-  Process::StrongPtr parentProcess{new DelayProcess{3000.0}};
-  Process::StrongPtr childProcess{new DelayProcess{2000.0}};
-  parentProcess->attachChild(std::move(childProcess));
-  processRunner.attachProcess(std::move(parentProcess));
-
-  ActorID actorID{0}, actorID2{0};
-  SDL_Log("sizeof(actorID) = %d", sizeof(actorID));
-  SDL_Log("actorID==actorID2 = %d", actorID==actorID2);
-  actorID.incrementVersion();
-  SDL_Log("actorID==actorID2 = %d", actorID==actorID2);
-
-  // test lua
-  bool luaHasError = scriptManager.runLua("function aLuaFunc(s)\nFW.debug('aLuaFucCalled!' .. s)\nend");
-  std::cout << "Lua has error? " << luaHasError << "\n";
-
-  scriptManager.getFunction("aLuaFunc")("an arg from c++");
-
-  scriptManager
-    .beginNamespace()
-      .beginClass<TestClassForLua>("TestClass")
-        .addConstructor<void(*)(int)>()
-        .addFunction("someMethod", &TestClassForLua::someMethod)
-      .endClass()
-      .addFunction("useTestClass", &useTestClassForLua)
-    .endNamespace();
-
-  scriptManager.runLua("local x = FW.TestClass(42)\nFW.debug(x:someMethod('SomeString', 1000))\nFW.useTestClass(x)\n");
-
-  scriptManager.runLua("function luaUseTestClass(x)\nFW.debug(x:someMethod('In luaUseTestClass', 666))\nreturn 33333\nend");
-  TestClassForLua xx{999};
-  scriptManager.getFunction("luaUseTestClass")(&xx);
-
-  scriptManager
-    .beginNamespace()
-      .beginClass<MyData1>("MyData1")
-        .addConstructor<void(*)(int, int)>()
-        .addProperty("x", &MyData1::x)
-        .addProperty("y", &MyData1::y)
-      .endClass()
-      .addFunction("useMyData1FromCPP", useMyData1FromLua)
-    .endNamespace();
-  MyData1 someMyData1{3, 5};
-  scriptManager.runLua("function useMyData1(x)\nFW.debug('Received MyData1! ' .. x.x .. ', ' .. x.y)\nend");
-  scriptManager.getFunction("useMyData1")(&someMyData1);
-  scriptManager.runLua("local x = FW.MyData1(7, 99)\nFW.useMyData1FromCPP(x)\nFW.debug('cpp messed things up: ' .. x.x .. ', ' .. x.y)\n");
-
-
-  scriptManager
-    .beginNamespace()
-      .beginNamespace("Event")
-        .beginClass<MyData1>("MyData1")
-          .addConstructor<void(*)(int, int)>()
-          .addProperty("x", &MyData1::x)
-          .addProperty("y", &MyData1::y)
-        .endClass()
-        .beginNamespace("EventBus")
-          .addFunction("emitMyData1", std::function<void(const MyData1&)>(std::bind(&EventBus::emit<MyData1>, &eventBus, std::placeholders::_1)))
-          .addFunction("subscribeMyData1", std::function<EventBus::EventListenerID(luabridge::LuaRef)>(std::bind(&EventBus::luaSubscribe<MyData1>, &eventBus, std::placeholders::_1)))
-          .addFunction("unsubscribeMyData1", std::function<void(EventBus::EventListenerID)>(std::bind(&EventBus::unsubscribe<MyData1>, &eventBus, std::placeholders::_1)))
-        .endNamespace()
-      .endNamespace()
-    .endNamespace();
-
-  eventBus.subscribe<MyData2>(std::bind(&MyListenerObject::myListenMethod, &obj, std::placeholders::_1));
-  eventBus.emit<MyData2>({"NotCalled"});
-  eventBus.emit<MyData1>({1, 2});
-  eventBus.subscribe<MyData1>(myListener1);
-  eventBus.emit<MyData2>({"NotCalled2"});
-  eventBus.emit<MyData1>({3, 4});
-  eventBus.subscribe<MyData2>(myListener2);
-  eventBus.emit<MyData2>({"Called!!"});
-  eventBus.emit<MyData1>({5, 6});
-  auto id2 = eventBus.subscribe<MyData1>(myListener3);
-  eventBus.emit<MyData2>({"Called Again!!"});
-  eventBus.unsubscribe<MyData1>(id2);
-  eventBus.unsubscribe<MyData1>(id2);
-  eventBus.unsubscribe<MyData2>(id2);
-  eventBus.emit<MyData1>({7, 8});
-
-  std::cout << "type in c++ = " << Event<MyData1>::type() << "\n";
-  scriptManager.runLua("function dataReceiver(data)\nprint('data received in lua: (' .. data.x .. ', ' .. data.y .. ')')\nend");
-  scriptManager.runLua("dataReceiver(FW.Event.MyData1(999, 999))");
-  scriptManager.runLua("listenerID = FW.Event.EventBus.subscribeMyData1(dataReceiver)");
-  scriptManager.runLua("FW.Event.EventBus.emitMyData1(FW.Event.MyData1(7000, 8000))");
-  eventBus.emit(MyData1{4634, 35645});
-
-  scriptManager.runLua("print('unsubscribing: ')\nprint(listenerID)\nFW.Event.EventBus.unsubscribeMyData1(listenerID)\nprint('success')");
-  eventBus.emit(MyData1{100, 100});
-  scriptManager.runLua("FW.Event.EventBus.emitMyData1(FW.Event.MyData1(77, 77))");
-
-  #include "Script/Scripts/EventBus.h"
-  scriptManager.runLua(std::string(src_Script_Scripts_EventBus_lua, src_Script_Scripts_EventBus_lua + src_Script_Scripts_EventBus_lua_len));
-
-  scriptManager.runLua("print('subscribe', EventBus.subscribeCustom)\nEventBus.subscribeCustom('myLuaEvent', function(data)\nprint('myLuaEvent received', data.xyz)\nend)\nprint('success')\n");
-  scriptManager.runLua("EventBus.emitCustom('myLuaEvent', { xyz = 444222 })");
-
-  initialized = true;
-}
-
 void FW::App::update() {
-  if (!initialized) {
-    init();
-    return;
-  }
   clock.update();
-  double dt = clock.dt();
-
   keyboardInput.update();
   mouseInput.update();
-  processRunner.update(dt);
 
-  if (keyboardInput.isClicked(KeyboardInput::Key::P)) {
-    std::cout << "P is clicked. music stopped\n";
-    audio.stopMusic();
-  }
-  if (keyboardInput.isClicked(KeyboardInput::Key::K)) {
-    if (audio.isPlayingMusic()) {
-      std::cout << "music is already playing :)\n";
-    } else {
-      std::cout << "K is clicked. music played\n";
-      audio.playMusic(*music);
-    }
-  }
-  
-  if (mouseInput.isClicked()) {
-    std::cout << "Clicked : (" << mouseInput.getX() << ", " << mouseInput.getY() << ")\n";
-    audio.playSound(*sound);
-  }
-
-  // for temp testing
-  graphics.prepareDraw();
-  graphics.prepareDrawSprite();
-  spriteSheet->prepareDraw();
-  graphics.drawSprite(*sprite);
-  spriteSheet->finishDraw();
-  graphics.finishDrawSprite();
+  std::cout << "App updating so empty\n";
 }
--- src/App/App.h
+++ src/App/App.h
@@ -9,13 +9,6 @@
 #include "Input/MouseInput.h"
 #include "Script/ScriptManager.h"
 
-// for temp testing
-#include "Event/EventBus.h"
-#include "Process/ProcessRunner.h"
-#include "File/RemoteFile.h"
-#include "Process/DelayProcess.h"
-#include <memory>
-
 namespace FW {
   class App {
   public:
@@ -31,8 +24,6 @@ namespace FW {
     App& operator=(const App&) =delete;
     ~App();
 
-    void init();
-
     void update();
 
   private:
@@ -43,17 +34,7 @@ namespace FW {
     Audio audio;
     KeyboardInput keyboardInput;
     MouseInput mouseInput;
-    EventBus eventBus;
-    RemoteFile remoteFile;
     Clock clock;
-    ProcessRunner processRunner;
-    bool initialized{false};
-
-    // for temp testing
-    std::unique_ptr<Sound> sound;
-    std::unique_ptr<Music> music;
-    std::unique_ptr<SpriteSheet> spriteSheet;
-    std::unique_ptr<Sprite> sprite;
   };
 }
 
